5efb0e9e8b87b44ea79063e31703f650d10312d9
vikasreddy11 <vikasreddy1090@gmail.com>
2026-04-08 10:26:45 +0530

    Dataset

--- sentiment_An.py
+++ sentiment_An.py
@@ -18,3 +18,15 @@ def vocab(datasets,min_frq=3):
             vocab[word]=len(word)
     return vocab
 
+#Imdb dataset
+class IMDBdataset(torch.utils.data.Dataset):
+    def __init__(self,data,vocab):
+        self.data=[]
+        self.vocab=vocab
+        unk=vocab['UNK']
+
+        for sample in data:
+            tokens=tokenizer(sample)
+            indices=[vocab.get(t,unk) for t in tokens]
+            labels=sample['label']
+            self.data.append(torch.tensor((indices,dtype=t)))
