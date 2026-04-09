b2f74db499fba561cbbdd57a29ba465cd68ea4c9
vikasreddy11 <vikasreddy1090@gmail.com>
2026-04-09 19:31:28 +0530

    Added Dataset and padding

--- Sentiment_RNN.py
+++ Sentiment_RNN.py
@@ -20,4 +20,32 @@ def build_vocab(dataset,min_frq=3):
     return vocab
 
 
+#Dataset
+class IMDBdataset(torch.utils.data.Dataset):
+    def __init__(self,data,vocab):
+        self.data=[]
+        self.vocab=vocab
+        unk=vocab['<unk>']
+        for sample in data:
+            tokens=tokenizer(sample['text'])
+            indices=[vocab.get(t,unk) for t in tokens]
+            label=sample['label']
+            self.data.append((torch.tensor(indices,dtype=torch.long),label))
+
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        return self.data[index]
+    
+#padding 
+def collate_fn(batch):
+    text,label=zip(*batch)
+    padded=torch.nn.utils.rnn.pad_sequence(text,batch_first=True,padding_value=0)
+    lengths=torch.tensor(len(t) for t in text)
+    indices=torch.tensor(label,dtype=torch.float)
+    return padded,lengths,indices
+
+#RNN
 
